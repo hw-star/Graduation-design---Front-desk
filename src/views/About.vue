c671899bdda0cab3eed7f008f269957a0db870da
hw-star <3111314916@qq.com>
2021-05-28 18:26:37 +0800

    修复个人中心以及个人活动列表不显示政策文件的bug

--- src/views/About.vue
+++ src/views/About.vue
@@ -19,6 +19,9 @@
             <li>
               <router-link to="/navigation">导航</router-link>
             </li>
+            <li>
+              <router-link to="/policy">政策文件</router-link>
+            </li>
           </ul>
         </div>
         <div class="nav_set_right">
